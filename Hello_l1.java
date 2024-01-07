ab0879ac80cd223ccbaf01ae4d75ed9fcedbe735
Boogiepop0208 <egnilxk@gmail.com>
2024-01-06 21:11:34 -0500

    Initial commit

---
+++ Hello_l1.java
@@ -0,0 +1,27 @@
+/**
+ * Hello
+ */
+public class Hello_l1 {
+    public static void main(String a[])
+    {   
+        // literals
+
+
+        int num1 = 0b101;
+        // 二进制，5
+
+        int num2 = 0x7E;
+        // 十六进制，126
+
+        int num3 = 10_00_00_000;
+        // 下划线帮助区分0，实际上还是100000000
+
+        System.out.println("Hello World");
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+    
+}
+
+
